4d68c0ba4430cb475dd55d3be712c4943a25333b
asswin-123 <64268036+asswin-123@users.noreply.github.com>
2022-12-15 22:34:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-import conversion_modules
+import json
+from pandas.io.json import json_normalize
+import pandas
 
 if __name__ == "__main__":
-    
-    path = str(input("Path of JSON file :"))
-    sheetname  = str(input("Name of sheet (optional) :"))
-    output_file = str(input("Path of Output Excel :"))
-    if(sheetname == ""):
+    jsonpath = str(input("path to json file :"))
+    outpath = str(input("output path of excel file :"))
+    sheetname = str(input("enter the sheetname (optional)"))
+    if sheetname == "":
         sheetname = "sheet1"
-    conversion_modules.json_toXlsx(path,output_file,sheetname)
+    json_toXlsx(jsonpath,outpath,sheetname)
+
+def json_toXlsx(Json_path,output_excel,sheetname):
+    #checking if the output has extension with it
+    if ".xlsx" not in output_excel:
+        output_excel = output_excel+".xlsx"
+    #normalize the json
+    try:
+        with open(Json_path) as file:
+            data = json.load(file)
+        normalizedJson = json_normalize(data)
+        normalizedJson.to_excel(output_excel,verbose=True,sheet_name=sheetname,encoding='utf-8')
+    except FileNotFoundError as identifier:
+        print("incorrect file path")
     
     
